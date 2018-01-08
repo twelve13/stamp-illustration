ffb1ced6f05b9fa52ff43b1e051905ce7fe80f04
Caroline Chen <carolinecaroline@gmail.com>
2018-01-07 19:45:48 -0500

    style button

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const divs = document.getElementsByTagName("DIV");
 const button = document.getElementById("button");
 
 function show() {
-	console.log("clicked")
+	button.remove();
 	for(let i=0; i<divs.length; i++){
 		setTimeout(function(){divs[i].classList.add("showme")}, i*200);
 	}
