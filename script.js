a08bbcd743ac328701bb0ae86e99f173488e97da
Caroline Chen <carolinecaroline@gmail.com>
2018-02-25 18:54:09 -0500

    replace button with onload

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 const divs = document.getElementsByTagName("DIV");
 
-const button = document.getElementById("button");
-
-function show() {
-	button.remove();
+function illustrate(){
 	for(let i=0; i<divs.length; i++){
 		setTimeout(function(){divs[i].classList.add("showme")}, i*200);
 	}
-}
+};
 
-button.addEventListener("click", show)
