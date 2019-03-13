3f971391f28f07be17a24a04a2e56c438987e7a1
Caroline Chen <carolinecaroline@gmail.com>
2019-03-13 12:18:45 -0400

    wrap setTimeout in anonymous function for IE issues

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const divs = document.getElementsByTagName("DIV");
 
 function illustrate(){
 	for(let i=0; i<divs.length; i++){
-		setTimeout(function(){divs[i].classList.add("showme")}, i*200);
+		(function(i){
+			setTimeout(function(){divs[i].classList.add("showme")}, i*200);
+		})(i);
 	}
 };
 
